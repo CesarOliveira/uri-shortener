08016614ed3f49bd59d5038846d68fbb4733dc0a
Cesar Oliveira <cesar.ap.oliveira@gmail.com>
2020-05-21 08:41:32 -0300

    Add test for Link model

--- api/spec/models/link_spec.rb
+++ api/spec/models/link_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'fields' do
+    it do
+      is_expected.to have_db_column(:id).of_type(:integer)
+      is_expected.to have_db_column(:title).of_type(:string)
+      is_expected.to have_db_column(:identifier).of_type(:string)
+      is_expected.to have_db_column(:destination_url).of_type(:string)
+      is_expected.to have_db_column(:created_at).of_type(:datetime)
+      is_expected.to have_db_column(:updated_at).of_type(:datetime)
+    end
+  end
 end
