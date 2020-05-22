7818120cd28f743bd3586a7418c058e278405901
Cesar Oliveira <cesar.ap.oliveira@gmail.com>
2020-05-22 08:37:16 -0300

    Implements the logic to deal with hash generator when creates a shorted link

--- api/app/domains/link/create.rb
+++ api/app/domains/link/create.rb
@@ -5,6 +5,16 @@ class Link::Create
   end
 
   def save
-    @link_repository.create(@params)
+    @link_repository.create(@params.merge!(identifier: generate_unique_identifier))
   end
+
+  private
+
+    def generate_unique_identifier(size_hash = ENV['INITIAL_SIZE_OF_HASH'].to_i)
+      unique_hash = SecureRandom.alphanumeric(size_hash)
+
+      return unique_hash unless @link_repository.find_by(identifier: unique_hash)
+
+      generate_unique_identifier(size_hash + 1)
+    end
 end
