7672f697d162d7c9efa34d1a5613e8a4c136b1bc
Cesar Oliveira <cesar.ap.oliveira@gmail.com>
2020-05-23 18:30:17 -0300

    Add method in LinkRepository to consult a link by its identifier and caches it

--- api/app/repositories/link_repository.rb
+++ api/app/repositories/link_repository.rb
@@ -1,5 +1,6 @@
 module LinkRepository
   extend self
+  include CacheHelper
 
   def create(params)
     Link.create!(params)
@@ -7,5 +8,11 @@ module LinkRepository
 
   def find_by(*args)
     Link.find_by(*args)
+  end
+
+  def find_by_identifier(identifier)
+    CacheHelper.fetch(identifier, 1.day) do
+      Link.find_by(identifier: identifier)
+    end
   end
 end
