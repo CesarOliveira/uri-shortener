b2fad0253a8f52db3367e6f2ec2d85aa58f67221
Cesar Oliveira <cesar.ap.oliveira@gmail.com>
2020-05-23 15:53:37 -0300

    Add test to find_by method to LinkRepository

--- api/spec/repositories/link_repository_spec.rb
+++ api/spec/repositories/link_repository_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe LinkRepository, type: :repository do
   let(:data) do
     {
-      destination_url: Faker::Number.number(digits: 1)
+      destination_url: Faker::Internet.url
     }
   end
 
@@ -19,6 +19,18 @@ RSpec.describe LinkRepository, type: :repository do
 
     it 'should call the create! method on the Link model' do
       expect(Link).to have_received(:create!).with(data)
+    end
+  end
+
+  describe '.find_by' do
+    before do
+      allow(Link).to receive(:find_by).with(data)
+
+      described_class.find_by(data)
+    end
+
+    it 'should call the find_by method on the Link model' do
+      expect(Link).to have_received(:find_by).with(data)
     end
   end
 end
