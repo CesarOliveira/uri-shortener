efb16fc01d6d46cc55163bed9eeb1aacde9f8532
Cesar Oliveira <cesar.ap.oliveira@gmail.com>
2020-05-23 17:06:40 -0300

    Improve test of models

--- api/spec/integration/models/hit_spec.rb
+++ api/spec/integration/models/hit_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Hit, type: :model do
+
+  let!(:hit) { Hit.new }
+
   describe 'fields' do
     it do
       is_expected.to have_db_column(:id).of_type(:integer)
@@ -8,6 +11,12 @@ RSpec.describe Hit, type: :model do
       is_expected.to have_db_column(:link_id).of_type(:integer)
       is_expected.to have_db_column(:created_at).of_type(:datetime)
       is_expected.to have_db_column(:updated_at).of_type(:datetime)
+    end
+  end
+
+  describe 'associations' do
+    it 'should validate if it belong to link' do
+      expect(hit).to belong_to :link
     end
   end
 end
--- api/spec/integration/models/link_spec.rb
+++ api/spec/integration/models/link_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
+
+  let!(:link) { Link.new }
+
   describe 'fields' do
     it do
       is_expected.to have_db_column(:id).of_type(:integer)
@@ -9,6 +12,18 @@ RSpec.describe Link, type: :model do
       is_expected.to have_db_column(:destination_url).of_type(:string)
       is_expected.to have_db_column(:created_at).of_type(:datetime)
       is_expected.to have_db_column(:updated_at).of_type(:datetime)
+    end
+  end
+
+  describe 'associations' do
+    it 'should validate if it belong to hits' do
+      expect(link).to have_many :hits
+    end
+  end
+
+  describe 'validations' do
+    it 'should validate the presence of destination_url' do
+      expect(link).to validate_presence_of :destination_url
     end
   end
 end
