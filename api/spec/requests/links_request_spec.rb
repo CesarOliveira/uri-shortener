79ce69374c570c536aceddc66129ce603f5b7ccb
Cesar Oliveira <cesar.ap.oliveira@gmail.com>
2020-05-23 16:46:07 -0300

    Improve link request test to deal with all possibilities

--- api/spec/requests/links_request_spec.rb
+++ api/spec/requests/links_request_spec.rb
@@ -45,14 +45,19 @@ RSpec.describe "Links", type: :request do
     end
 
     context 'when an error occurs during save' do
+      let(:error_message) { Faker::Lorem.word }
 
       before do
-        allow(Link).to receive(:create!).and_return(false)
+        allow(Link::Create).to receive(:new).and_raise(StandardError, error_message)
         post '/api/links', params: data
       end
 
       it 'must return status unprocessable_entity' do
         expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 'must match json data error message' do
+        expect(JSON.parse(response.body)['errors']).to eq(error_message)
       end
     end
   end
