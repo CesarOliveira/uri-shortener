33ae56b277e831a7ace79b361478244188c3de9c
Cesar Oliveira <cesar.ap.oliveira@gmail.com>
2020-05-24 15:47:02 -0300

    Improve domain to find the link

--- api/app/domains/link/find.rb
+++ api/app/domains/link/find.rb
@@ -1,11 +1,16 @@
 class Link::Find
   def initialize(params, link_repository)
-    @params = params
+    @params = params.to_h
     @link_repository = link_repository
   end
 
   def run
-    return @link_repository.find_by_identifier(@params['identifier']) if @params['identifier'].present?
+    return @link_repository.find_by_identifier(@params['identifier']) if find_only_by_identifier?
     @link_repository.find_by(@params)
   end
+
+  private
+    def find_only_by_identifier?
+      @params.size == 1 && @params['identifier'].present?
+    end
 end
