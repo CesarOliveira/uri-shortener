0ac33bdecc540e4e4b8354aea2530aa638e69b8c
Cesar Oliveira <cesar.ap.oliveira@gmail.com>
2020-05-24 21:44:47 -0300

    Implement a method to generate a unique token

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+
+  def generate_unique_token(identifier)
+    identifier_md5 = Digest::MD5.hexdigest identifier
+    time = DateTime.now.to_f
+    secure_random_hash = SecureRandom.hex(rand(7..42))
+    (Digest::MD5.hexdigest "#{secure_random_hash}-#{time}-#{identifier_md5}")
+  end
 end
