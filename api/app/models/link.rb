7c44e6e64355a9ef780ec6844b85f538c7cecd9c
Cesar Oliveira <cesar.ap.oliveira@gmail.com>
2020-05-26 07:51:24 -0300

    Add query to calculate hits and unique hits of a link

--- api/app/models/link.rb
+++ api/app/models/link.rb
@@ -1,4 +1,19 @@
 class Link < ApplicationRecord
   has_many :hits
   validates :destination_url, presence: true
+
+  scope :count_hits, -> do
+    query = "
+      SELECT
+        lk.id,
+        lk.title,
+        lk.identifier,
+        lk.destination_url,
+        (SELECT COUNT(*) FROM hits ht WHERE ht.link_id = lk.id) as total_hits,
+        (SELECT COUNT(DISTINCT ht.identifier) FROM hits ht WHERE ht.link_id = lk.id) as unique_hits
+      FROM links lk
+      ORDER BY total_hits DESC"
+
+    Link.find_by_sql(query)
+  end
 end
