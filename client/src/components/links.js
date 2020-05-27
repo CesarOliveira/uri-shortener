4666fba3987c61c5a237c4789296e0d3aadc64b9
Cesar Oliveira <cesar.ap.oliveira@gmail.com>
2020-05-27 08:34:48 -0300

    Improve list ofMost Accessed Links

--- client/src/components/links.js
+++ client/src/components/links.js
@@ -1,21 +1,42 @@
 import React from 'react'
 
 const Links = ({links}) => {
-    return (
-        <div>
-            <center><h1>Most Accessed Link</h1></center>
-            {links.map((link) => (
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">{link.title}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{link.destination_url}</h6>
-                        <p class="card-text">{link.total_hits}</p>
-                        <p class="card-text">{link.unique_hits}</p>
-                    </div>
-                </div>
-            ))}
+  return (
+      <div>
+        <center><h1>Most Accessed Link</h1></center>
+
+        <div class="row">
+        <div class="col-lg-12">
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">#Id</th>
+                <th scope="col">Title</th>
+                <th scope="col">Identifier</th>
+                <th scope="col">Destination</th>
+                <th scope="col">Total Hits</th>
+                <th scope="col">Unique Hits</th>
+              </tr>
+            </thead>
+            <tbody>
+              {links.map((link) => (
+                <tr>
+                  <th scope="row">{link.id}</th>
+                  <td>{link.title}</td>
+                  <td>
+                    <a target="_blank" href={`http://shortener.cesaroliveira.com.br/${link.identifier}`}>http://shortener.cesaroliveira.com.br/{link.identifier}</a>
+                  </td>
+                  <td>{link.destination_url}</td>
+                  <td>{link.total_hits}</td>
+                  <td>{link.unique_hits}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-    )
+        </div>
+      </div>
+  )
 };
 
 export default Links
