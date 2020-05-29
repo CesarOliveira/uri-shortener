7bd8d2e2883f786c57d2fb4291ad4153615a8889
Cesar Oliveira <cesar.ap.oliveira@gmail.com>
2020-05-28 22:14:11 -0300

    When starts the container check what kind of service it is and launch the correct proccess hutch or rails

--- api/post-up.sh
+++ api/post-up.sh
@@ -1,4 +1,8 @@
-rm -f tmp/pids/server.pid
 bundle install --quiet
-rails s -b 0.0.0.0 -p 3000
 
+if [ "$SERVICE_TYPE" = "worker" ]; then
+  sh wait-rabbit-and-connect.sh
+else
+  rm -f tmp/pids/server.pid
+  rails s -b 0.0.0.0 -p 3000
+fi
